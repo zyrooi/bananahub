cbbac03ccec63e89829d0894d7e2f3e6cd0c0220
zyrooi <kietblox7777@gmail.com>
2025-03-27 20:42:36 +0700

    Update babanadead.lua

--- babanadead.lua
+++ babanadead.lua
@@ -2,6 +2,8 @@ if game.GameId == 7018190066 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/zyrooi/bananahub/refs/heads/main/deadcode.lua"))()
 elseif game.GameId == 5830262966 then
     loadstring(game:HttpsGet("https://raw.githubusercontent.com/zyrooi/bananahub/refs/heads/main/Squid.lua.txt"))()
+elseif game.GameId == 994732206 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/zyrooi/quanly/refs/heads/main/gay.lua"))()
 else
     -- Nếu không phải game hỗ trợ, kick người chơi
     game.Players.LocalPlayer:Kick("[ BANANA HUB ]")
